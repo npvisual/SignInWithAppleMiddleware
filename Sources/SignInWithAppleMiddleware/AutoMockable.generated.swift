204199877419a5ca498b6fba72cf84a3363e27bc
Nick <npvisual@users.noreply.github.com>
2020-11-20 20:07:06 -0600

    Frist checkin
    This should be all we need for SIWA for the moment.

---
+++ Sources/SignInWithAppleMiddleware/AutoMockable.generated.swift
@@ -0,0 +1,13 @@
+// Generated using Sourcery 1.0.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// swiftlint:disable all
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
+
+
